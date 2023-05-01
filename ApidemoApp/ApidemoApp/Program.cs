abbbc85baa4efa5ec4ce2a877a828fb0b3adde17
madhub <b.madhu@outlook.com>
2023-05-01 17:44:59 +0530

    api app with otel trace support.

---
+++ ApidemoApp/ApidemoApp/Program.cs
@@ -0,0 +1,43 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Diagnostics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracerProviderBuilder =>
+        tracerProviderBuilder
+            .AddSource(DiagnosticsConfig.ActivitySource.Name)
+            .ConfigureResource(resource => resource
+                .AddService(DiagnosticsConfig.ServiceName))
+            .AddAspNetCoreInstrumentation()
+             .AddOtlpExporter( )
+            .AddConsoleExporter());
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+public static class DiagnosticsConfig
+{
+    public const string ServiceName = "ApidemoApp";
+    public static ActivitySource ActivitySource = new ActivitySource(ServiceName);
+}
